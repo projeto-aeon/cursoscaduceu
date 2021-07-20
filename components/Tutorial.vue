b6354453f6cf9ed95c44c3107ccb7bf1ebe45829
AlbertoAlfredo <raioalfredo@gmail.com>
2021-07-20 20:13:37 -0300

    configuraçoes iniciais

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,7 +1,7 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-    <h1>Em construção</h1>
+  <div class="relative flex flex-col items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <h1>Em construção</h1><br/>
     <h2>Em breve uma nova experiência de publicidade e desenvolvimento!</h2>
   </div>
 </template>
