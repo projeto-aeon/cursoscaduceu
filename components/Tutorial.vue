0221b97d3414d492c1ed0947d00cdad62731a9ec
AlbertoAlfredo <raioalfredo@gmail.com>
2021-07-20 20:29:54 -0300

    configuraçoes iniciais

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -3,5 +3,6 @@
   <div class="relative flex flex-col items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
     <h1>Em construção</h1><br/>
     <h2>Em breve uma nova experiência de publicidade e desenvolvimento!</h2>
+    <img src="~assets/images/caduceucartao1.jpg" alt="">
   </div>
 </template>
