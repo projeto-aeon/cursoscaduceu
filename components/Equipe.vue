710dd07fafdecd125f0c3bbad00037fd322ceab1
AlbertoAlfredo <raioalfredo@gmail.com>
2021-07-27 18:15:01 -0300

    Arruamando tamanho dos nomes

--- components/Equipe.vue
+++ components/Equipe.vue
@@ -5,7 +5,7 @@
   <div style="display: flex; flex-direction: row; justify-content: space-between;" >
   <div style="padding: 2vw;" v-for="(pessoa, index) in this.equipe" :key="index">
     <div>
-    <h1 class="z-50" style="width: 10vw; margin-bottom: -4vw; position: relative; color: var(--thirdy-color)"><b>{{pessoa.nome}}</b></h1>
+    <h1 class="z-50" style="width: 14vw; margin-bottom: -4vw; position: relative; color: var(--thirdy-color)"><b>{{pessoa.nome}}</b></h1>
       <nuxt-img class="z-10" style="width: 20vw; height: 20vw; border-radius: 50vw; position: relative;" :src="pessoa.img" />
       <div style="display: flex; flex-direction: row; justify-content: center;">
         <a :href="pessoa.face"><svg class="icon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><rect fill="none" height="24" width="24"/><path d="M22,12c0-5.52-4.48-10-10-10S2,6.48,2,12c0,4.84,3.44,8.87,8,9.8V15H8v-3h2V9.5C10,7.57,11.57,6,13.5,6H16v3h-2 c-0.55,0-1,0.45-1,1v2h3v3h-3v6.95C18.05,21.45,22,17.19,22,12z"/></svg></a>
