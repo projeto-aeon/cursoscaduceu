8554f8ea07bd35e0f278ba07209284cece6b4fbf
AlbertoAlfredo <raioalfredo@gmail.com>
2021-07-28 21:52:36 -0300

    Efeito hover

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon1.ico' },
       {rel:"stylesheet", href:"https://rsms.me/inter/inter.css"}
 
     ]
